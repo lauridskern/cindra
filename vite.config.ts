7dd670389961d9a5b96d3ae79fe9da85fdc291d7
Laurids Kern <me@lauridskern.com>
2026-04-16 17:21:20 +0200

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from 'node:path'
+
 import { defineConfig } from 'vitest/config'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
@@ -5,6 +7,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
